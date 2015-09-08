4be3f75ebe923690a8c21c786777b4a13cda7695
brzone <brzone@126.com>
2015-09-08 16:49:18 +0800

    消除告警

--- My-Brzone-Algorithm/src/com/facebook/brzone/list/MyArrayList.java
+++ My-Brzone-Algorithm/src/com/facebook/brzone/list/MyArrayList.java
@@ -15,10 +15,12 @@ public class MyArrayList<T> {
 	
 	private static final int DEFAULT_INIT_SIZE = 10;
 	
+	@SuppressWarnings("unchecked")
 	public MyArrayList(){
 		data  = (T[])new Object[DEFAULT_INIT_SIZE];
 	}
 	
+	@SuppressWarnings("unchecked")
 	public MyArrayList(int size) {
 		
 		if(size < 1) {
@@ -34,6 +36,7 @@ public class MyArrayList<T> {
 	 * 当调用add(T t)方法时候，需要确定数组是否可以增加保存一个原始，意思是说，是否还有一个空位来保存，如果没有空位的话，
 	 * 重新初始化一个新的数组，并扩大相应容量，把旧数组数据copy到新数组中
 	 */
+	@SuppressWarnings("unchecked")
 	private void ensureCapacity(){
 		
 		//是否需要扩容
