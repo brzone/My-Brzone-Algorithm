985abc50ddb3262be5e79337d014b3d439bcb68d
brzone <brzone@126.com>
2015-09-10 14:27:03 +0800

    撒发放

--- My-Brzone-Algorithm/src/com/facebook/brzone/list/AAAA.java
+++ My-Brzone-Algorithm/src/com/facebook/brzone/list/AAAA.java
@@ -3,7 +3,8 @@ package com.facebook.brzone.list;
 public class AAAA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 中国人
+		// TODO Auto-generated method stub 
+		//发发发发生的范文网
 
 	}
 
